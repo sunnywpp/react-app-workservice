9c03b07806e4ebeb37c77da2ed5b25cb36e0e3d8
wang.pp <wang.pp@neusoft.com>
2020-04-23 11:06:29 +0800

    Updates

---
+++ precache-manifest.d043a6a8b7a9179aa84a8d11cc07fc6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0c7b2541251dc6bf3d491622410634",
+    "url": "/react-app-workservice/index.html"
+  },
+  {
+    "revision": "395e9c22f33d02621416",
+    "url": "/react-app-workservice/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "03f15272a9f295c8ea5f",
+    "url": "/react-app-workservice/static/js/2.7357d3f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-workservice/static/js/2.7357d3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395e9c22f33d02621416",
+    "url": "/react-app-workservice/static/js/main.2f119026.chunk.js"
+  },
+  {
+    "revision": "a056dd70c8b874ac03f0",
+    "url": "/react-app-workservice/static/js/runtime-main.8c5526b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-workservice/static/media/logo.5d5d9eef.svg"
+  }
+]);
